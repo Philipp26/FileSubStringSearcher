fe2e612befc3d95f1940d011aeba5221e24a7286
Your Name <you@example.com>
2021-06-13 16:22:09 +0300

    Изменен файл конфигурации

--- FileSubStringSearcher/Program.cs
+++ FileSubStringSearcher/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
 
 namespace FileSubStringSearcher
@@ -13,14 +10,28 @@ namespace FileSubStringSearcher
         [Obsolete]
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите строку для поиска в файлах:");
+            var pattern = Console.ReadLine();
+
             var startPath = ConfigurationSettings.AppSettings["startFolder"];
+            var filePathes = new List<string>();
 
             var getSupTreeFiles = Directory
-                .GetFiles(startPath, "*.*", SearchOption.AllDirectories)
-                .Where(s => s.EndsWith(".sql"))
-                .ToArray(); 
+                .GetFiles(startPath, ConfigurationSettings.AppSettings["extensions"], SearchOption.AllDirectories);
 
+            foreach(var path in getSupTreeFiles)            
+                if (File.ReadAllText(path).Contains(pattern))                
+                    filePathes.Add(new FileInfo(path).FullName);
 
+            Console.WriteLine("\r\nСписок файлов содержащих строку:");
+
+            if (filePathes.Count == 0)
+                Console.WriteLine("Не найдено");
+            else
+                foreach (var item in filePathes)
+                    Console.WriteLine(item);
+
+            Console.ReadKey();
         }
     }
 }
