a5ebbc81e2309410995e4164be9492ffe593c745
Your Name <you@example.com>
2021-06-13 15:39:47 +0300

    Добавьте файлы проекта.

---
+++ FileSubStringSearcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace FileSubStringSearcher
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            var startPath = ConfigurationSettings.AppSettings["startFolder"];
+
+            var getSupTreeFiles = Directory
+                .GetFiles(startPath, "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".sql"))
+                .ToArray(); 
+
+
+        }
+    }
+}
